c3cb34d1863ee04e281e83bccc62c548b207feb4
parvej <parvej.alam624@gmail.com>
2023-01-18 00:54:03 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,182 @@
+// Select the canvas
+const canvas = document.getElementById("myGame");
+const context = canvas.getContext("2d");
+
+// Drow rectangle function
+function drowRect(x,y,w,h,color){
+    context.fillStyle = color
+    context.fillRect(x,y,w,h)
+}
+
+// Computer paddle
+const com = {
+    x: canvas.width/2 - 50/2,
+    y: 10,
+    width: 50,
+    height: 10,
+    color: "white",
+    score: 0
+}
+
+// User paddle
+const user = {
+    x: canvas.width/2 - 50/2,
+    y: canvas.height - 10 - 10,
+    width: 50,
+    height: 10,
+    color: "white",
+    score: 0
+}
+
+// Center line
+function centerLine(){
+    context.beginPath()
+    context.setLineDash([10])
+    context.moveTo(0,canvas.height/2)
+    context.lineTo(canvas.width,canvas.height/2)
+    context.strokeStyle = "white" 
+    context.stroke()
+}
+
+// Drow a Circle
+function drowCircle(x,y,r,color){
+    context.fillStyle = color
+    context.beginPath()
+    context.arc(x,y,r,0,Math.PI*2,false)
+    context.closePath()
+    context.fill()
+}
+
+// Create a ball
+const ball = {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    radius: 10,
+    speed: 1,
+    velocityX: 5,
+    velocityY: 5,
+    color: "white"
+}
+
+//  Scores
+function drowText(text,x,y,color){
+    context.fillStyle = color
+    context.font = "32px josefin sans"
+    context.fillText(text,x,y)
+}
+
+// render the Game
+function render(){
+
+    // Make canvas
+    drowRect(0,0,400,600,"black");
+
+    // computer paddle
+    drowRect(com.x,com.y,com.width,com.height,com.color)
+    // User paddle
+    drowRect(user.x,user.y,user.width,user.height,user.color)
+    
+    // Center Line
+    centerLine()
+
+    // create a ball
+    drowCircle(ball.x,ball.y,ball.radius,ball.color)
+
+    // Scores of com and user
+    drowText(com.score,20,canvas.height/2 - 30)
+    drowText(user.score,20,canvas.height/2 + 50)
+}
+
+// control the user paddle
+canvas.addEventListener("mousemove", movepaddle);
+function movepaddle(e){
+    let rect = canvas.getBoundingClientRect();
+    user.x = e.clientX - rect.left - user.width/2;
+}
+
+// colliosion detection
+function colliosion(b,p){
+    b.top = b.y - b.radius;
+    b.bottom = b.y + b.radius;
+    b.left = b.x - b.radius;
+    b.right = b.x +b.radius;
+
+    p.top = p.y;
+    p.bottom = p.y + p.height;
+    p.left = p.x;
+    p.right = p.x + p.width;
+
+    return p.right > b.left && p.left < b.right && b.bottom > p.top && b.top < p.bottom;
+}
+
+// reset ball
+function resetBall(){
+    ball.x = canvas.width/2;
+    ball.y = canvas.height/2;
+
+    ball.speed = 1;
+    ball.velocityY = -ball.velocityY;
+}
+
+// Game over function
+function ShowGameOver(){
+    // hide canvas
+    canvas.style.display = "none";
+    const can = document.getElementById("can");
+    can.style.display = "none";
+    //container
+    const result = document.getElementById("result");
+    result.style.display = "block";
+}
+
+// update
+function update(){
+    ball.x += ball.velocityX*ball.speed;
+    ball.y += ball.velocityY*ball.speed;
+
+    //control the computer paddle
+    let computerLevel = 0.1;
+    com.x += (ball.x - (com.x + com.width/2)) + computerLevel;
+    if(ball.speed > 2){
+        com.x += ball.x + 100;
+    }
+
+    // reflect from wall
+    if(ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0){
+        ball.velocityX = -ball.velocityX;
+    }
+
+    // if colliosion happens
+    let player = (ball.y < canvas.height/2) ? com : user;
+    if(colliosion(ball,player)){
+        ball.velocityY = -ball.velocityY;
+        ball.speed += 0.1;
+    }
+
+    // points
+    if(ball.y - ball.radius < 0){
+        user.score++
+        resetBall()
+    }else if(ball.y + ball.radius > canvas.height){
+        com.score++
+        resetBall()
+    }
+
+    // Game over
+    if(user.score > 4 || com.score > 4){
+        clearInterval(loop);
+        ShowGameOver();
+    }
+    
+}
+
+//start the game
+function start(){
+    update();
+    render()
+}
+
+// loop
+const loop = setInterval(start, 1000/50);
+
+
